9c0d4eb2a0c23ce5fb4e1ebf41933268775d123d
Emil Sadek <esadek@users.noreply.github.com>
2024-12-20 23:19:46 -0800

    Fix: formatting

--- astro.config.ts
+++ astro.config.ts
@@ -1,6 +1,5 @@
-// @ts-check
-import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
+import { defineConfig } from "astro/config";
 
 export default defineConfig({
   site: "https://emilsadek.com",
