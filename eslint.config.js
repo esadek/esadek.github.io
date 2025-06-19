06957609ec84d506cda5726903eb818d5aef50d5
Emil Sadek <esadek@users.noreply.github.com>
2025-06-19 21:58:28 +0200

    Add: eslint

---
+++ eslint.config.js
@@ -0,0 +1,14 @@
+import js from "@eslint/js";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+import { defineConfig, globalIgnores } from "eslint/config";
+import eslintPluginAstro from 'eslint-plugin-astro';
+
+
+export default defineConfig([
+  { files: ["**/*.{js,mjs,cjs,ts,mts,cts}"], plugins: { js }, extends: ["js/recommended"] },
+  { files: ["**/*.{js,mjs,cjs,ts,mts,cts}"], languageOptions: { globals: globals.browser } },
+  tseslint.configs.recommended,
+  ...eslintPluginAstro.configs.recommended,
+  globalIgnores([".astro/*"])
+]);
